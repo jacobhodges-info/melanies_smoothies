9d94938d90725c951a0e0075c8b832405990a601
jacobhodges-info <jacob.hodges@infocentric.com.au>
2026-08-07 11:18:30 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
